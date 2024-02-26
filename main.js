1812390566fbdb5fb35920be124cfe3ebe715fec
Meenu Yadav <yadav.meenu2023@gmail.com>
2024-02-26 14:07:48 +0530

    TextSpeech

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ function uploadFile() {
    var file = document.getElementById("input")
    var reader = new FileReader();
    reader.onload = function (e) {
-       var textArea = document.getElementById("textArea")
-       textArea.textContent = e.target.result;
+       var upload = document.getElementById("upload")
+       upload.textContent = e.target.result;
    }
    reader.readAsText(file.files[0])
    reader.addEventListener("loadend", handleEvent);
@@ -18,16 +18,12 @@ function handleEvent() {
    array.forEach(element => {
        span += `<span>${element}</span> `
    });
-   // console.log(para)
-   // console.log(span)
+
    var readText = new SpeechSynthesisUtterance();
-   // console.log(readText)
    para.innerHTML = span
    para.addEventListener("click", function (e) {
-       // console.log(e.target.innerHTML)
        e.target.classList.toggle("highlight")
        readText.text = e.target.innerHTML;
        window.speechSynthesis.speak(readText);
-       // console.log(readText.text)
    })
 }
