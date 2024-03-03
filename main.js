fc9ae9d242707928613971581d3961088544dc22
Meenu Yadav <yadav.meenu2023@gmail.com>
2024-03-03 11:20:50 +0530

    Text-speech

--- main.js
+++ main.js
@@ -1,29 +1,37 @@
 
 function uploadFile() {
-   var file = document.getElementById("input")
-   var reader = new FileReader();
-   reader.onload = function (e) {
-       var upload = document.getElementById("upload")
-       upload.textContent = e.target.result;
-   }
-   reader.readAsText(file.files[0])
-   reader.addEventListener("loadend", handleEvent);
+    var file = document.getElementById("input")
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        var upload = document.getElementById("upload")
+        upload.textContent = e.target.result;
+    }
+    reader.readAsText(file.files[0])
+    reader.addEventListener("loadend", handleEvent);
 }
 
 function handleEvent() {
-   let para = document.querySelector(".para")
-   let array = para.textContent.split(" ")
-   let span = ""
+    let para = document.querySelector(".para")
+    let array = para.textContent.split(" ")
+    let span = ""
 
-   array.forEach(element => {
-       span += `<span>${element}</span> `
-   });
-
-   var readText = new SpeechSynthesisUtterance();
-   para.innerHTML = span
-   para.addEventListener("click", function (e) {
-       e.target.classList.toggle("highlight")
-       readText.text = e.target.innerHTML;
-       window.speechSynthesis.speak(readText);
-   })
+    array.forEach(element => {
+        span += `<span>${element}</span> `
+    });
+    // console.log(para)
+    // console.log(span)
+    var readText = new SpeechSynthesisUtterance();
+    // console.log(readText)
+    para.innerHTML = span
+    para.addEventListener("click", function (e) {
+        // console.log(e.target.innerHTML)
+        e.target.classList.toggle("highlight")
+        readText.text = e.target.innerHTML;
+        window.speechSynthesis.cancel();
+        window.speechSynthesis.speak(readText);
+        // console.log(readText)
+    })
 }
+
+
+
